acb23356d7d1a5401ccf01502f84deba89fa8dfc
Sebastian <sebastian.malicki02@gmail.com>
2024-07-08 22:32:58 +0200

    added FeeTest and FeeBreakpointTest

---
+++ tests/Model/FeeTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace tests\Model;
+
+use PHPUnit\Framework\TestCase;
+use PragmaGoTech\Interview\Model\Fee;
+
+final class FeeTest extends TestCase
+{
+    private Fee $fee;
+
+    public function testFeeValueObjectConstruct(): void
+    {
+        $fee = new Fee(5, 11);
+
+        $this->assertInstanceOf(Fee::class, $fee);
+    }
+
+    public function testAsFloat(): void
+    {
+        $this->assertSame(5.0, $this->fee->asFloat());
+    }
+
+    public function testRoundUpReturnFeeValueObject(): void
+    {
+        $this->assertInstanceOf(Fee::class, $this->fee->roundUp());
+    }
+
+    public function testRoundUpCorrectly(): void
+    {
+        $roundedUpFee = $this->fee->roundUp();
+
+        $this->assertSame(9.0, $roundedUpFee->asFloat());
+    }
+
+    protected function setUp(): void
+    {
+        $this->fee = new Fee(5, 11);
+    }
+}
---
+++ tests/Provider/FeeBreakpointTest.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace tests\Provider;
+
+use PHPUnit\Framework\TestCase;
+use PragmaGoTech\Interview\Provider\FeeBreakpoint;
+
+final class FeeBreakpointTest extends TestCase
+{
+    private array $term24Breakpoints;
+    private array $term12Breakpoints;
+
+    public function testFee12Breakpoints(): void
+    {
+        $this->assertSame($this->term12Breakpoints, FeeBreakpoint::getBreakpoints(12));
+    }
+
+    public function testFee24Breakpoints(): void
+    {
+        $this->assertSame($this->term24Breakpoints, FeeBreakpoint::getBreakpoints(24));
+    }
+
+    protected function setUp(): void
+    {
+        $this->term12Breakpoints = [
+            1000 => 50,
+            2000 => 90,
+            3000 => 90,
+            4000 => 115,
+            5000 => 100,
+            6000 => 120,
+            7000 => 140,
+            8000 => 160,
+            9000 => 180,
+            10000 => 200,
+            11000 => 220,
+            12000 => 240,
+            13000 => 260,
+            14000 => 280,
+            15000 => 300,
+            16000 => 320,
+            17000 => 340,
+            18000 => 360,
+            19000 => 380,
+            20000 => 400
+        ];
+
+        $this->term24Breakpoints = [
+            1000 => 70,
+            2000 => 100,
+            3000 => 120,
+            4000 => 160,
+            5000 => 200,
+            6000 => 240,
+            7000 => 280,
+            8000 => 320,
+            9000 => 360,
+            10000 => 400,
+            11000 => 440,
+            12000 => 480,
+            13000 => 520,
+            14000 => 560,
+            15000 => 600,
+            16000 => 640,
+            17000 => 680,
+            18000 => 720,
+            19000 => 760,
+            20000 => 800
+        ];
+    }
+}
